65cbeb2de31e493ddd87ef3eb7bdf67f13ad5b7a
Ritvik <ritvik.sardana11@gmail.com>
2021-03-26 01:05:06 +0530

    CSS left, all done

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#output");
+
+// var url = "https://api.funtranslations.com/translate/minion.json"
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function urlCreator(text){
+    return url + "?" + "text=" +text;
+}
+
+
+function errorHandler(error){
+    console.log("Error Occured",error)
+}
 
 function clickEventHandler() {
-    console.log("Clicked");
-}
+    // console.log("Clicked");
+    // console.log(txtInput.value)
+    // txtOutput.innerHTML = "Translated " + txtInput.value;
+    var text = txtInput.value
+
+    fetch(urlCreator(text))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        txtOutput.innerText = translatedText;
+        console.log(translatedText)
+            })
+    .catch(errorHandler)
+};
 
  btnTranslate.addEventListener("click",clickEventHandler)
